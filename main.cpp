3d48f124475cdfeeaa0ccf5fcdb09cbcbec68a84
Jay Smit <jaytyu@gmail.com>
2018-10-07 16:03:40 +0200

    yoyo

--- main.cpp
+++ main.cpp
@@ -29,7 +29,6 @@ int main()
 		//return 1;
 	}
 
-
     std::cout<<"1\n";
     std::this_thread::sleep_for(std::chrono::seconds(1));
 	
@@ -43,4 +42,29 @@ int main()
 
     std::cout<<"2\n";
     std::this_thread::sleep_for(std::chrono::seconds(1));
+
+
+    //----- READ BYTES -----
+	length = 4;			//<<< Number of bytes to read
+	if (read(file_i2c, buffer, length) != length)		//read() returns the number of bytes actually read, if it doesn't match then an error occurred (e.g. no response from the device)
+	{
+		//ERROR HANDLING: i2c transaction failed
+		printf("Failed to read from the i2c bus.\n");
+	}
+	else
+	{
+		printf("Data read: %s\n", buffer);
+	}
+
+	
+	//----- WRITE BYTES -----
+	buffer[0] = 0x00;
+	buffer[1] = 0x70;
+	length = 2;			//<<< Number of bytes to write
+	if (write(file_i2c, buffer, length) != length)		//write() returns the number of bytes actually written, if it doesn't match then an error occurred (e.g. no response from the device)
+	{
+		/* ERROR HANDLING: i2c transaction failed */
+		printf("Failed to write to the i2c bus.\n");
+	}
+
 }
