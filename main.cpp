25e2d907d9fb857a59f23b907ae2bd3e1e3abd63
Jay Smit <jaytyu@gmail.com>
2018-10-08 19:48:35 +0200

    untested function for 1 measurement every sec

--- main.cpp
+++ main.cpp
@@ -43,27 +43,50 @@ int main()
     std::cout<<"2\n";
     std::this_thread::sleep_for(std::chrono::seconds(1));
 
-    //----- READ BYTES -----
-	length = 4;			//<<< Number of bytes to read
-	if (read(file_i2c, buffer, length) != length)		//read() returns the number of bytes actually read, if it doesn't match then an error occurred (e.g. no response from the device)
-	{
-		//ERROR HANDLING: i2c transaction failed
-		printf("Failed to read from the i2c bus.\n");
-	}
-	else
-	{
-		printf("Data read: %s\n", buffer);
-	}
 
-	
-	//----- WRITE BYTES -----
-	buffer[0] = 0x00;
-	buffer[1] = 0x70;
-	length = 2;			//<<< Number of bytes to write
+	buffer[0] = 0x3c;
+	buffer[1] = 0x00;
+	buffer[2] = 0x70;
+	length = 3;			//<<< Number of bytes to write
 	if (write(file_i2c, buffer, length) != length)		//write() returns the number of bytes actually written, if it doesn't match then an error occurred (e.g. no response from the device)
-	{
-		/* ERROR HANDLING: i2c transaction failed */
 		printf("Failed to write to the i2c bus.\n");
-	}
 
+	buffer[0] = 0x3c;
+	buffer[1] = 0x01;
+	buffer[2] = 0xa0;
+	length = 3;			//<<< Number of bytes to write
+	if (write(file_i2c, buffer, length) != length)		//write() returns the number of bytes actually written, if it doesn't match then an error occurred (e.g. no response from the device)
+		printf("Failed to write to the i2c bus.\n");
+
+
+	while(true){
+		buffer[0] = 0x3c;
+		buffer[1] = 0x02;
+		buffer[2] = 0x01;
+		length = 3;			//<<< Number of bytes to write
+		if (write(file_i2c, buffer, length) != length)		//write() returns the number of bytes actually written, if it doesn't match then an error occurred (e.g. no response from the device)
+			printf("Failed to write to the i2c bus.\n");	
+
+		std::this_thread::sleep_for(std::chrono::milliseconds(6));
+
+		buffer[0] = 0x3d;
+		buffer[1] = 0x06;
+		length = 2;			//<<< Number of bytes to write
+		if (write(file_i2c, buffer, length) != length)		//write() returns the number of bytes actually written, if it doesn't match then an error occurred (e.g. no response from the device)
+			printf("Failed to write to the i2c bus.\n");
+
+		//----- READ BYTES -----
+		length = 6;			//<<< Number of bytes to read
+		if (read(file_i2c, buffer, length) != length)		//read() returns the number of bytes actually read, if it doesn't match then an error occurred (e.g. no response from the device)
+		{
+			//ERROR HANDLING: i2c transaction failed
+			printf("Failed to read from the i2c bus.\n");
+		}
+		else
+		{
+			printf("Data read: %s\n", buffer);
+		}
+
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+	}
 }
